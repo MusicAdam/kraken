9e9919a027338e6a1e5aa1e4697ff0494c02cecf
Adam Zielinski <zielinskia@wit.edu>
2015-04-12 16:54:55 -0400

    Testing build failure

--- core/src/com/gearworks/kraken/utils/Behavior.java
+++ core/src/com/gearworks/kraken/utils/Behavior.java
@@ -4,7 +4,7 @@ public interface Behavior {
 	/*
 	 * Called once when the behavior is attached to its parent, should set this.parent = parent
 	 */
-	public void onAttach(Object parent);
+	public void onAttach(Object parent)
 	/*
 	 * Called once when the behavior is detached from its parent, should set parent to null
 	 */
